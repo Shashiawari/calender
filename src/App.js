597e13975f9f3043ea44b359d5c7ace96b869377
Shashi Preetham Awari <109402358+Shashiawari@users.noreply.github.com>
2024-08-16 21:49:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CalendarApp from './Components/CalendarApp'
+import './Components/CalendarApp.css'
+
+const App = () => {
+  return (
+    <div className="container">
+      <CalendarApp />
+      <h1 className='port'  >Visit my portfolio <a target="_blank" rel="noreferrer" href='https://shashipreetham.netlify.app/'>click here</a></h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/CalendarApp.jsx
@@ -0,0 +1,265 @@
+import { useState } from "react";
+
+const CalendarApp = () => {
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const monthsOfYear = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const currentDate = new Date();
+
+  const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
+  const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+  const [selectedDate, setSelectedDate] = useState(currentDate);
+  const [showEventPopup, setShowEventPopup] = useState(false);
+  const [events, setEvents] = useState([]);
+  const [eventTime, setEventTime] = useState({ hours: "00", minutes: "00" });
+  const [eventText, setEventText] = useState("");
+  const [editingEvent, setEditingEvent] = useState(null);
+
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+
+  const prevMonth = () => {
+    setCurrentMonth((prevMonth) => (prevMonth === 0 ? 11 : prevMonth - 1));
+    setCurrentYear((prevYear) =>
+      currentMonth === 0 ? prevYear - 1 : prevYear
+    );
+  };
+
+  const nextMonth = () => {
+    setCurrentMonth((prevMonth) => (prevMonth === 11 ? 0 : prevMonth + 1));
+    setCurrentYear((prevYear) =>
+      currentMonth === 11 ? prevYear + 1 : prevYear
+    );
+  };
+
+  const handleDayClick = (day) => {
+    const clickedDate = new Date(currentYear, currentMonth, day);
+    const today = new Date();
+
+    if (clickedDate >= today || isSameDay(clickedDate, today)) {
+      setSelectedDate(clickedDate);
+      setShowEventPopup(true);
+      setEventTime({ hours: "00", minutes: "00" });
+      setEventText("");
+      setEditingEvent(null);
+    }
+  };
+
+  const isSameDay = (date1, date2) => {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  };
+
+  const handleEventSubmit = () => {
+    const newEvent = {
+      id: editingEvent ? editingEvent.id : Date.now(),
+      date: selectedDate,
+      time: `${eventTime.hours.padStart(2, "0")}:${eventTime.minutes.padStart(
+        2,
+        "0"
+      )}`,
+      text: eventText,
+    };
+
+    let updatedEvents = [...events];
+
+    if (editingEvent) {
+      updatedEvents = updatedEvents.map((event) =>
+        event.id === editingEvent.id ? newEvent : event
+      );
+    } else {
+      updatedEvents.push(newEvent);
+    }
+
+    updatedEvents.sort((a, b) => new Date(a.date) - new Date(b.date));
+
+    setEvents(updatedEvents);
+    setEventTime({ hours: "00", minutes: "00" });
+    setEventText("");
+    setShowEventPopup(false);
+    setEditingEvent(null);
+  };
+
+  const handleEditEvent = (event) => {
+    setSelectedDate(new Date(event.date));
+    setEventTime({
+      hours: event.time.split(":")[0],
+      minutes: event.time.split(":")[1],
+    });
+    setEventText(event.text);
+    setEditingEvent(event);
+    setShowEventPopup(true);
+  };
+
+  const handleDeleteEvent = (eventId) => {
+    const updatedEvents = events.filter((event) => event.id !== eventId);
+    setEvents(updatedEvents);
+  };
+
+  const handleTimeChange = (e) => {
+    const { name, value } = e.target;
+    setEventTime((prevTime) => ({
+      ...prevTime,
+      [name]: value.padStart(2, "0"),
+    }));
+  };
+
+  return (
+    <div className="calendar-app">
+      <div className="calendar">
+        <h1 className="heading">Calendar</h1>
+        <div className="navigate-date">
+          <h2 className="month">{monthsOfYear[currentMonth]},</h2>
+          <h2 className="year">{currentYear}</h2>
+          <div className="buttons">
+            <svg
+              onClick={prevMonth}
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-caret-left-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
+            </svg>
+            <svg
+              onClick={nextMonth}
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-caret-right-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+            </svg>
+          </div>
+        </div>
+        <div className="weekdays">
+          {daysOfWeek.map((day) => (
+            <span key={day}>{day}</span>
+          ))}
+        </div>
+        <div className="days">
+          {[...Array(firstDayOfMonth).keys()].map((_, index) => (
+            <span key={`empty-${index}`} />
+          ))}
+          {[...Array(daysInMonth).keys()].map((day) => {
+            const currentDay = new Date(currentYear, currentMonth, day + 1);
+            const isActive = events.some((event) =>
+              isSameDay(new Date(event.date), currentDay)
+            );
+
+            return (
+              <span
+                key={day + 1}
+                className={`${
+                  day + 1 === currentDate.getDate() &&
+                  currentMonth === currentDate.getMonth() &&
+                  currentYear === currentDate.getFullYear()
+                    ? "current-day"
+                    : ""
+                } ${isActive ? "active" : ""}`}
+                onClick={() => handleDayClick(day + 1)}
+              >
+                {day + 1}
+              </span>
+            );
+          })}
+        </div>
+      </div>
+      <div className="events">
+        {showEventPopup && (
+          <div className="event-popup">
+            <div className="time-input">
+              <div className="event-popup-time">Time</div>
+              <input
+                type="number"
+                name="hours"
+                min={0}
+                max={24}
+                className="hours"
+                value={eventTime.hours}
+                onChange={handleTimeChange}
+              />
+              <input
+                type="number"
+                name="minutes"
+                min={0}
+                max={60}
+                className="minutes"
+                value={eventTime.minutes}
+                onChange={handleTimeChange}
+              />
+            </div>
+            <textarea
+              placeholder="Enter Event Text (Maximum 60 Characters)"
+              value={eventText}
+              onChange={(e) => {
+                if (e.target.value.length <= 60) {
+                  setEventText(e.target.value);
+                }
+              }}
+            ></textarea>
+            <button className="event-popup-btn" onClick={handleEventSubmit}>
+              {editingEvent ? "Update Event" : "Add Event"}
+            </button>
+            <button
+              className="close-event-popup"
+              onClick={() => setShowEventPopup(false)}
+            >
+              <i className="bx bx-x"></i>
+            </button>
+          </div>
+        )}
+        {events.length === 0 ? (
+          <div className="no-events">No events for now</div>
+        ) : (
+          events.map((event, index) => (
+            <div className="event" key={index}>
+              <div className="event-date-wrapper">
+                <div className="event-date">{`${
+                  monthsOfYear[new Date(event.date).getMonth()]
+                } ${new Date(event.date).getDate()}, ${new Date(
+                  event.date
+                ).getFullYear()}`}</div>
+                <div className="event-time">{event.time}</div>
+              </div>
+              <div className="event-text">{event.text}</div>
+              <div className="event-buttons">
+                <i
+                  className="bx bxs-edit-alt"
+                  onClick={() => handleEditEvent(event)}
+                ></i>
+                <i
+                  className="bx bxs-trash"
+                  onClick={() => handleDeleteEvent(event.id)}
+                ></i>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    
+    </div>
+  );
+};
+
+export default CalendarApp;
